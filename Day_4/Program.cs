fdb0eb1a5d77b0b413ac1b1afc261299290918c5
youssef_74 <youssefessam1293@gmail.com>
2024-12-21 17:57:31 +0200

    Add project files.

---
+++ Day_4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Day_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region q1
+            Console.WriteLine("Enter Number");
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine(i);
+            }
+            #endregion
+        }
+    }
+}
